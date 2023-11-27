f9c491b43512cc74c15baa6c116ff9706472d9f7
Diane Barrie <diane.barrie@phs.scot>
2023-11-27 14:53:52 +0000

    server.r - remove pages/page_1 and functions/page_1_functions ui.r - remove tab Page 1 page_1.r - deleted

--- server.R
+++ server.R
@@ -10,11 +10,10 @@ server <- function(input, output, session) {
   # Get functions
   source(file.path("functions/core_functions.R"), local = TRUE)$value
   source(file.path("functions/intro_page_functions.R"), local = TRUE)$value
-  source(file.path("functions/page_1_functions.R"), local = TRUE)$value
+
   
   # Get content for individual pages
   source(file.path("pages/intro_page.R"), local = TRUE)$value
-  source(file.path("pages/page_1.R"), local = TRUE)$value
   source(file.path("pages/notes_page.R"), local = TRUE)$value
   
   # Get SERVER code for the individual pages
