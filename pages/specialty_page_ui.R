9886bee657c3e7f3b39483c7c0c14b950b58fd2f
natpol100 <natalie.polack@phs.scot>
2023-11-28 15:14:15 +0000

    specialty_page_ui: Added text to introduce page.Added instructions on how to use dropdowns. Added instructions about clicking on legend.

--- pages/specialty_page_ui.R
+++ pages/specialty_page_ui.R
@@ -6,13 +6,15 @@ tagList(
   
   fluidRow(
     h1("View data by NHS Board and Specialty"),
+    p("This page shows 4 charts: Referrals, Removals, Patients Seen and Patients Waiting. Data quality notes for each NHS Board can be found on the Notes tab.")
     
   ), 
   
   fluidRow(
     column(6,
+           p(em("Please select up to 4 NHS Boards.")),
            selectizeInput("board_specialty",
-                       "Select a Board (Up to 4 selections)",
+                       "Select NHS Board(s)",
                        choices = c("Scotland",
                                    "NHS Ayrshire & Arran",
                                    "NHS Borders",
@@ -36,6 +38,7 @@ tagList(
            )
     ),
     column(6,
+           p(em("Please select 1 Specialty.")),
            selectInput("specialty_specialty",
                        "Select a Specialty",
                        choices = unique(Join_all_long2$Specialty)
@@ -47,6 +50,7 @@ tagList(
   fluidRow(
     column(6,
            h2("Referrals: Select an indicator"),
+           p(em("Please select 1 Indicator.")),
            selectInput("refs_indicator",
                        "Select an Indicator",
                        choices = unique(Referrals$Indicator),
@@ -56,35 +60,39 @@ tagList(
            ),
     column(6,
            h2("Removals: Select an indicator"),
+           p(em("Please select 1 Indicator.")),
            selectInput("removals_indicator",
                        "Select an Indicator",
                        choices = unique(Removals$Indicator),
                        selected = "Removals from list",
            ),
            plotlyOutput("removals_specialty_plot")
-    )
+           )
   ),
-  
+  fluidRow(p(em("A single click on the legend line will remove it from the plot while a double click on the legend line will show just that line."))),
   fluidRow(
     column(6,
            h2("Seen: Select an indicator"),
+           p(em("Please select 1 Indicator.")),
            selectInput("seen_indicator",
                        "Select an Indicator",
                        choices = unique(Seen$Indicator),
                        selected = "Patients seen"
            ),
            plotlyOutput("seen_specialty_plot")
-    ),
+           ),
     column(6,
            h2("Waiting: Select an indicator"),
+           p(em("Please select 1 Indicator.")),
            selectInput("waiting_indicator",
                        "Select an Indicator",
                        choices = unique(Waiting$Indicator),
                        selected = "Patients waiting"
            ),
            plotlyOutput("waiting_specialty_plot")
-    )
-  )
+           )
+  ),
+  fluidRow(p(em("A single click on the legend line will remove it from the plot while a double click on the legend line will show just that line."))),
   
   
     
